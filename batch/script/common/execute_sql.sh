16bfe40ac3e9ec580b09eb37910b9b7a6a21721d
hdeg <76611318+exit1991@users.noreply.github.com>
2023-03-14 08:41:45 +0900

    SQL実行シェルのエラー判定方法を改善 #1

--- batch/script/common/execute_sql.sh
+++ batch/script/common/execute_sql.sh
@@ -78,24 +78,30 @@ if [ ! -f ${exec_sql_file_path} ]; then
 fi
 
 # SQL実行
-psql -d "${DB_NAME}" -f "${exec_sql_file_path}" ${DB_BIND} > "${STD_OUT_FILE}" 2> "${STD_ERR_FILE}"
+psql -d "${DB_NAME}" -f "${exec_sql_file_path}" --set ON_ERROR_STOP=on ${DB_BIND} > "${STD_OUT_FILE}" 2> "${STD_ERR_FILE}"
+# PSQLからの戻り値
+psql_return_code=$?
+
+# 標準出力をログへ出力
 if [ -s ${STD_OUT_FILE} ]; then
     log_msg ${INFO} "PSQL標準出力メッセージ...\n""$(cat ${STD_OUT_FILE})"
 else
     log_msg ${INFO} "PSQL標準出力メッセージ なし"
 fi
-
-# SQLエラーチェック
+# エラー出力をログへ出力
 if [ -s ${STD_ERR_FILE} ]; then
-    log_msg ${ERR} "PSQLエラー"
-    log_msg ${ERR} "PSQLエラーメッセージ...\n""$(cat ${STD_ERR_FILE})"
-    delete_std_out_file
-    log_msg ${ERR} "異常終了"
-    exit 1
+    log_msg ${INFO} "PSQLエラー出力メッセージ...\n""$(cat ${STD_ERR_FILE})"
 fi
 
 # 出力確認用ファイルの削除
 delete_std_out_file
+
+# SQLエラーチェック
+if [ ${psql_return_code} -ne 0 ]; then
+    log_msg ${ERR} "PSQLエラー 戻り値：${psql_return_code}"
+    log_msg ${ERR} "異常終了"
+    exit ${psql_return_code}
+fi
 
 log_msg ${INFO} "正常終了"
 exit 0
